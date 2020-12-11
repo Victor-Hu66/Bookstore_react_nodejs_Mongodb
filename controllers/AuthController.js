be7032c31cdf4079a2ee06074515f05edd442490
Ahmet <scienceworker34@gmail.com>
2020-12-11 13:45:00 +0300

    add register and login routers

---
+++ controllers/AuthController.js
@@ -0,0 +1,9 @@
+exports.authRegister= (req, res) => {
+    res.send("Register completed")
+}
+
+
+exports.authLogin = (req, res) => {
+    res.send("login completed")
+}
+
--- routes/router.js
+++ routes/router.js
@@ -2,12 +2,30 @@ const { Router } = require("express");
 const express = require("express");
 
 const router = express.Router();
+const AuthRouter = require("./AuthRouter");
+const ProfileRouter = require("./ProfileRouter");
+const BookRouter = require("./BookRouter");
 
 
-router.get("/", (req, res) => {
-    console.log("Api page")
-    res.send("Api page")
-})
+// Only /api endpoint
+
+/**
+ * @route /api/auth
+ * @desc Route for Auth
+ */
+router.use("/auth", AuthRouter)
+
+/**
+ * @route /api/profile
+ * @desc Route for Profile
+ */
+// router.use("/profile", ProfileRouter)
+/**
+ * @route /api/book
+ * @desc Route for Book
+ */
+// router.use("/book", BookRouter)
+
 
 
 module.exports = router
