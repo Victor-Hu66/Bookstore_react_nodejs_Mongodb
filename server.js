0e00f29c5e2e9efb4bf1f8fdad117d2459c554f7
Ahmet <scienceworker34@gmail.com>
2020-12-17 17:46:12 +0300

    add production env

--- server.js
+++ server.js
@@ -13,7 +13,16 @@ connectDB();
 app.use( "/api", router )
 
 
-app.listen(port, () => {
+// production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
+
+
+ app.listen(port, () => {
     console.log(`I m listening on port ${port}`)
 })
 
